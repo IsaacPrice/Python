ac9c11427aac29d0855f1d4dae18d99b9a637bf4
IsaacPrice <iprice25250@gmail.com>
2023-07-27 08:33:53 -0400

    added another attempt at machine learning

--- Deep Learning/DIY/test.py
+++ Deep Learning/DIY/test.py
@@ -2,11 +2,15 @@ import pandas as pd
 import numpy as np
 from HMAI import *
 
-df = pd.read_csv("WineQuality.csv")
+df = pd.read_csv("Salary_Data.csv")
 df.dropna(inplace=True)
 
-model = Sequential (df, {'Target' : 'quality', 'Type' : 'softmax'})
-model.add(Dense(input_dim=32485, output_dim=32, activation='relu'))
+# This splits the dataframe into the ratio we needs
+train_df, test_df = split_dataframe(df, train_ratio=.8)
+
+model = Sequential (train_df, test_df, {'Target' : 'Salary', 'Type' : 'softmax'})
+model.add(Dense(input_dim=200, output_dim=32, activation='relu'))
 model.add(Dense(input_dim=32, output_dim=4, activation='softmax'))
 
-model.run(10000)
+model.fit(10000)
+print(model.evaluate())
